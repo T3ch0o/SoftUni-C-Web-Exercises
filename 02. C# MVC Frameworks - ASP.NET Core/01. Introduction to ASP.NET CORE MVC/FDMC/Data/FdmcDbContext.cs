93f0ad0fd3e22241e1ba8dbd72c09e8421613a8e
T3ch0o <trottimusbg@gmail.com>
2018-11-09 09:58:52 +0200

    Initial commit

---
+++ 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Data/FdmcDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace FDMC.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FdmcDbContext : IdentityDbContext
+    {
+        public FdmcDbContext(DbContextOptions<FdmcDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
