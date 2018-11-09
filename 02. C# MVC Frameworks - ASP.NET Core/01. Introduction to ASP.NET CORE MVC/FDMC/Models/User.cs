879878084b8248d3e3865ce46378b1407973b10f
T3ch0o <trottimusbg@gmail.com>
2018-11-09 10:32:03 +0200

    [User] Added model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace FDMC.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class User : IdentityUser
+    {
+        public string Nickname { get; set; }
+
+        public override string UserName { get; set; }
+    }
+}
