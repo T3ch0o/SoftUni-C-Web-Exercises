2c39ff86e49e7b740ca62cac9e2b20b453a2f192
T3ch0o <trottimusbg@gmail.com>
2018-11-12 10:01:13 +0200

    [CatService] Implemented logic for GetAllCats.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Services/CatService.cs
@@ -0,0 +1,23 @@
+﻿namespace FDMC.Services
+{
+    using System.Linq;
+
+    using FDMC.Data;
+    using FDMC.Models;
+    using FDMC.Services.Interfaces;
+
+    public class CatService : ICatService
+    {
+        private readonly FdmcDbContext _db;
+
+        public CatService(FdmcDbContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<Cat> GetAllCats()
+        {
+            return _db.Cats;
+        }
+    }
+}
