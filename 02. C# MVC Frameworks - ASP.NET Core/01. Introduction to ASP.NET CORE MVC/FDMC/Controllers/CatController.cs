0418bd061712ad26ba65d0224017b5961aab7745
T3ch0o <trottimusbg@gmail.com>
2018-11-12 10:38:38 +0200

    [CatController] Implemented logic for creating cat.

--- 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Controllers/CatController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Controllers/CatController.cs
@@ -1,6 +1,30 @@
 ﻿namespace FDMC.Controllers
 {
-    public class CatController
+    using FDMC.Models.ViewModels;
+    using FDMC.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CatController : Controller
     {
+        private readonly ICatService _catService;
+
+        public CatController(ICatService catService)
+        {
+            _catService = catService;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AddCatViewModel model)
+        {
+            _catService.AddCat(model);
+
+            return Redirect("/");
+        }
     }
 }
