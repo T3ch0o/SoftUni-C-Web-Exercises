68574f5cc595fcd0ee5b2be4d06447f2b33dc279
T3ch0o <trottimusbg@gmail.com>
2018-11-12 10:02:48 +0200

    [HomeController] Removed other actions.

--- 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Controllers/HomeController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Controllers/HomeController.cs
@@ -1,41 +1,21 @@
 ﻿namespace FDMC.Controllers
 {
-    using System.Diagnostics;
+    using FDMC.Services.Interfaces;
 
     using Microsoft.AspNetCore.Mvc;
 
-    using FDMC.Models;
-
     public class HomeController : Controller
     {
+        private readonly ICatService _catService;
+
+        public HomeController(ICatService catService)
+        {
+            _catService = catService;
+        }
+
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
