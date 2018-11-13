e8b75a833bc124106cf3ed690765071a77564228
T3ch0o <trottimusbg@gmail.com>
2018-11-13 15:19:09 +0200

    [Fdmc] Application file changes.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/01. Introduction to ASP.NET CORE MVC/FDMC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,88 @@
+﻿namespace FDMC.Areas.Identity.Pages.Account
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading.Tasks;
+
+    using FDMC.Models;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+
+        public RegisterModel(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<RegisterModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty] public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                User user = new User { UserName = Input.Username, Email = Input.Email, Nickname = Input.Nickname};
+                IdentityResult result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    string code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    string callbackUrl = Url.Page("/Account/ConfirmEmail", null, new { userId = user.Id, code }, Request.Scheme);
+
+                    await _signInManager.SignInAsync(user, false);
+                    return LocalRedirect(returnUrl);
+                }
+
+                foreach (IdentityError error in result.Errors) ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(10, ErrorMessage = "Username must be at least 1 characters long.")]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            public string Nickname { get; set; }
+        }
+    }
+}
