168d354239113911a5c5397e50d1c8021833df15
T3ch0o <trottimusbg@gmail.com>
2018-11-13 15:21:35 +0200

    [ApplicationUser] Extending model for Identity User.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Chushka.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+}
