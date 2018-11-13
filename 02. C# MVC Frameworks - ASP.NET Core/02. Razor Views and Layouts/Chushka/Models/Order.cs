63be8d663f88d49d23a1ffcfaeab416dae470ab3
T3ch0o <trottimusbg@gmail.com>
2018-11-13 16:32:21 +0200

    [Order] Created model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace Chushka.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+
+        public string ClientId { get; set; }
+
+        [ForeignKey(nameof(ClientId))]
+        public ApplicationUser Client { get; set; }
+
+        public DateTime OrderOn { get; set; }
+    }
+}
