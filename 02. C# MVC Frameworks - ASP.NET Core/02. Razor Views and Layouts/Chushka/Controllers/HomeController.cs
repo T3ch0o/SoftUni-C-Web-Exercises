44d282e8911876bd9433bf19ac8d2f135ca87136
T3ch0o <trottimusbg@gmail.com>
2018-11-14 20:54:26 +0200

    [HomeController] Injected ProductService to get all products and pass them to the view.

--- 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Controllers/HomeController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Controllers/HomeController.cs
@@ -1,11 +1,28 @@
 ﻿namespace Chushka.Controllers
 {
+    using System.Collections.Generic;
+
+    using Chushka.Models;
+    using Chushka.Services.Interfaces;
+
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : Controller
     {
+        private readonly IProductService _productService;
+
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View(_productService.GetProducts());
+            }
+
             return View();
         }
     }
