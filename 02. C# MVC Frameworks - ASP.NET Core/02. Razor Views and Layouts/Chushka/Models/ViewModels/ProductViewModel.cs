a62ac11504aee42230fc55c0e3d28fbe16407818
T3ch0o <trottimusbg@gmail.com>
2018-11-14 23:45:15 +0200

    [ProductViewModel] Added Id property.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Chushka.Models.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        public string SelectedFoodType { get; set; }
+    }
+}
