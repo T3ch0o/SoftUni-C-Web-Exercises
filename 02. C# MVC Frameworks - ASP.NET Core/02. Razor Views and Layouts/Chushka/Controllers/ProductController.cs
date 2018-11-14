706634218928c2c3ee92447ad8cc3d5fbef1f791
T3ch0o <trottimusbg@gmail.com>
2018-11-14 16:18:35 +0200

    [ProductController] Added render action for create view.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿namespace Chushka.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProductController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
