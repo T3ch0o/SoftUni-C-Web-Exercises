82c0cfc242e4e47322470828bb25778a41badd3d
T3ch0o <trottimusbg@gmail.com>
2018-11-14 13:47:01 +0200

    [Seeder] Implemented seed method wich adds administrator role.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Utilities/Seeder.cs
@@ -0,0 +1,20 @@
+﻿namespace Chushka.Utilities
+{
+    using System;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class Seeder
+    {
+        public static async void Seed(IServiceProvider serviceProvider)
+        {
+            RoleManager<IdentityRole> roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Administrator"));
+            }
+        }
+    }
+}
