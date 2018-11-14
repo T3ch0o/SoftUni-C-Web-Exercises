18108bc16ea4625dc1b2cfe9c4b2a6ea20606fe3
T3ch0o <trottimusbg@gmail.com>
2018-11-14 22:47:03 +0200

    [OrderSerivice] Added method for create order.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,7 @@
+﻿namespace Chushka.Services
+{
+    public interface IOrderService
+    {
+        void CreateOrder(string userId, int productId);
+    }
+}
---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿namespace Chushka.Services
+{
+    using System;
+
+    using Chushka.Data;
+    using Chushka.Models;
+
+    public class OrderService : IOrderService
+    {
+        private readonly ChushkaDbContext _db;
+
+        public OrderService(ChushkaDbContext db)
+        {
+            _db = db;
+        }
+
+        public void CreateOrder(string userId, int productId)
+        {
+            Order order = new Order
+            {
+                ProductId = productId,
+                ClientId = userId,
+                OrderOn = DateTime.Now
+            };
+
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+        }
+    }
+}
