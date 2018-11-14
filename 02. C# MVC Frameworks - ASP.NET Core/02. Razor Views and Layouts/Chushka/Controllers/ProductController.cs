cfd0ff59e57981683cf9a9b03c882797eae5fed1
T3ch0o <trottimusbg@gmail.com>
2018-11-14 20:55:03 +0200

    [ProductController] Added action for create product.

--- 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Controllers/ProductController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Controllers/ProductController.cs
@@ -1,14 +1,30 @@
 ﻿namespace Chushka.Controllers
 {
-    using System.Collections.Generic;
+    using Chushka.Models.ViewModels;
+    using Chushka.Services.Interfaces;
 
     using Microsoft.AspNetCore.Mvc;
 
     public class ProductController : Controller
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public RedirectResult Create(CreateProductViewModel model)
+        {
+            _productService.AddProduct(model);
+
+            return Redirect("/");
         }
     }
 }
