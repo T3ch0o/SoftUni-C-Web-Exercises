78b9fd3718ad6170bafaa8efa7be7784fe0b67f3
T3ch0o <trottimusbg@gmail.com>
2018-11-14 21:37:15 +0200

    [ProductService] Added methods for creating and geting products.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿namespace Chushka.Services.Interfaces
+{
+    using System.Collections.Generic;
+
+    using Chushka.Models;
+    using Chushka.Models.ViewModels;
+
+    public interface IProductService
+    {
+        IEnumerable<Product> GetProducts();
+
+        void AddProduct(CreateProductViewModel model);
+    }
+}
---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Services/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chushka.Services
+{
+    using Chushka.Data;
+    using Chushka.Models;
+    using Chushka.Models.Enums;
+    using Chushka.Models.ViewModels;
+    using Chushka.Services.Interfaces;
+
+    public class ProductService : IProductService
+    {
+        private readonly ChushkaDbContext _db;
+
+        public ProductService(ChushkaDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products;
+        }
+
+        public void AddProduct(CreateProductViewModel model)
+        {
+            if (Enum.TryParse(model.SelectedFoodType, out ProductType productType))
+            {
+                Product product = new Product
+                {
+                        Name = model.Name,
+                        Description = model.Description,
+                        Price = model.Price,
+                        Type = productType
+                };
+
+                _db.Products.Add(product);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
