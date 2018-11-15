45eea1413da9bc55c685e19f71479b1083da028b
T3ch0o <trottimusbg@gmail.com>
2018-11-15 13:02:26 +0200

    [OrdersController] Added Index action for displaying all orders.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿namespace Chushka.Controllers
+{
+    using System.Collections.Generic;
+
+    using Chushka.Models;
+    using Chushka.Services;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class OrdersController : Controller
+    {
+        private readonly IOrderService _orderServices;
+
+        public OrdersController(IOrderService orderServices)
+        {
+            _orderServices = orderServices;
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Index()
+        {
+            List<Order> orders = _orderServices.GetOrders();
+
+            return View(orders);
+        }
+    }
+}
