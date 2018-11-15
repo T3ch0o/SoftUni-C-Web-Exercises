a055d128385720f7b2fc6bd42997e7247e54cab8
T3ch0o <trottimusbg@gmail.com>
2018-11-15 13:06:36 +0200

    [Order] Changed model properties.

--- 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Models/Order.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/02. Razor Views and Layouts/Chushka/Models/Order.cs
@@ -8,13 +8,9 @@
         public int Id { get; set; }
 
         public int ProductId { get; set; }
-
-        [ForeignKey(nameof(ProductId))]
-        public virtual Product Product { get; set; }
+        public Product Product { get; set; }
 
         public string ClientId { get; set; }
-
-        [ForeignKey(nameof(ClientId))]
         public ApplicationUser Client { get; set; }
 
         public DateTime OrderOn { get; set; }
