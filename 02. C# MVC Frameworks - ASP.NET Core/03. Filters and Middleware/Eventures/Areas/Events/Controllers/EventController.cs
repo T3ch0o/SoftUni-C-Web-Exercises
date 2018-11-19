ffb585b7ee8a211ad2fcd3d243157ea76ee54637
T3ch0o <trottimusbg@gmail.com>
2018-11-19 21:44:20 +0200

    [EventController] Implemented logic to get all events and pass them to the view.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Controllers/EventController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Controllers/EventController.cs
@@ -1,5 +1,9 @@
 ﻿namespace Eventures.Areas.Events.Controllers
 {
+    using System.Collections.Generic;
+
+    using Eventures.Areas.Event.ViewModels;
+    using Eventures.Models;
     using Eventures.Services.Interfaces;
 
     using Microsoft.AspNetCore.Mvc;
@@ -15,9 +19,21 @@
 
         public IActionResult Index()
         {
+            IEnumerable<Event> events = _eventService.All();
+            List<EventViewModel> eventViewModels = new List<EventViewModel>();
 
+            foreach (Event @event in events)
+            {
+                eventViewModels.Add(new EventViewModel
+                {
+                    Name = @event.Name,
+                    Place = @event.Place,
+                    Start = @event.Start,
+                    End = @event.End
+                });
+            }
 
-            return View();
+            return View(eventViewModels);
         }
     }
 }
