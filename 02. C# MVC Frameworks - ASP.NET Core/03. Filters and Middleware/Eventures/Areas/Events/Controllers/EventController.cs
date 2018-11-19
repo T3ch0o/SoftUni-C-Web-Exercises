3a8c1ca8285dd46bb6dbc0a96c517f8786ef9df0
T3ch0o <trottimusbg@gmail.com>
2018-11-19 16:44:42 +0200

    [EventController] Injected EventService.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Controllers/EventController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Controllers/EventController.cs
@@ -1,9 +1,18 @@
 ﻿namespace Eventures.Areas.Events.Controllers
 {
+    using Eventures.Services.Interfaces;
+
     using Microsoft.AspNetCore.Mvc;
 
     public class EventController : Controller
     {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
         public IActionResult Index()
         {
 
