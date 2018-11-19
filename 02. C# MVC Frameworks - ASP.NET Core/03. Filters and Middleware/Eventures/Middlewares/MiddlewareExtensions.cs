31156ac1779d8fd1c6a2969aa77041745111c975
T3ch0o <trottimusbg@gmail.com>
2018-11-19 16:45:13 +0200

    [MiddlewareExtensions] Added SeedDataMiddleware.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Eventures.Middlewares
+{
+    using Microsoft.AspNetCore.Builder;
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSeedDataMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SeedDataMiddleware>();
+        }
+    }
+}
