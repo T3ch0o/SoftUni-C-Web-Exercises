ba97fb6c247691b36f2a4e8eb6ac8fc56e75059a
T3ch0o <trottimusbg@gmail.com>
2018-11-19 14:30:53 +0200

    [LoginViewModel] Added Remember me property.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ViewModels/LoginViewModel.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ViewModels/LoginViewModel.cs
@@ -12,5 +12,8 @@
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
