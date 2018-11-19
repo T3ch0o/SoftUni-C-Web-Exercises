8373d6df8558a758f2c4449f355147e86180b66c
T3ch0o <trottimusbg@gmail.com>
2018-11-19 15:20:07 +0200

    [EventViewModel] Added model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/ViewModels/EventViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Eventures.Areas.Event.ViewModels
+{
+    using System;
+
+    public class EventViewModel
+    {
+        public string Name { get; set; }
+
+        public string Place { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
