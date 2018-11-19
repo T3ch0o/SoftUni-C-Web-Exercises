9506b740c693d93f1ce28e008f9f1c86e497833e
T3ch0o <trottimusbg@gmail.com>
2018-11-19 14:30:23 +0200

    [ApplicationUser] Added required properties.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ApplicationUser.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ApplicationUser.cs
@@ -4,6 +4,10 @@
 
     public class ApplicationUser : IdentityUser
     {
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UniversalCitizenNumber { get; set; }
     }
 }
