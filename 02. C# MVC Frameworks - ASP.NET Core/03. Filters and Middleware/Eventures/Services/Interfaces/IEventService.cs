7db5b1e7d7ed6303c6f7b579044cb0a5e4c57abb
T3ch0o <trottimusbg@gmail.com>
2018-11-20 22:19:18 +0200

    [IEventService] Added Create method.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/Interfaces/IEventService.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/Interfaces/IEventService.cs
@@ -1,10 +1,14 @@
 ﻿namespace Eventures.Services.Interfaces
 {
     using System.Collections.Generic;
+
+    using Eventures.Areas.Event.ViewModels;
     using Eventures.Models;
 
     public interface IEventService
     {
         IEnumerable<Event> All();
+
+        void Create(EventViewModel model);
     }
 }
