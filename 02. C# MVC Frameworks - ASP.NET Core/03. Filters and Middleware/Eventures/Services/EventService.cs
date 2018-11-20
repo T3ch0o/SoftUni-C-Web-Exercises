d5b50f2cdd5256501fc4a205f0772d9a5b05ec43
T3ch0o <trottimusbg@gmail.com>
2018-11-20 22:18:53 +0200

    [EventService] Added Create method which adds event entity to to database.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/EventService.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/EventService.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Generic;
 
+    using Eventures.Areas.Event.ViewModels;
     using Eventures.Data;
     using Eventures.Models;
     using Eventures.Services.Interfaces;
@@ -18,6 +19,22 @@
         public IEnumerable<Event> All()
         {
             return _db.Events;
+        }
+
+        public void Create(EventViewModel model)
+        {
+            Event eventureEvent = new Event
+            {
+                Name = model.Name,
+                Place = model.Place,
+                Start = model.Start,
+                End = model.End,
+                TotalTickets = model.TotalTickets,
+                TicketPrice = model.TicketPrice
+            };
+
+            _db.Events.Add(eventureEvent);
+            _db.SaveChanges();
         }
     }
 }
