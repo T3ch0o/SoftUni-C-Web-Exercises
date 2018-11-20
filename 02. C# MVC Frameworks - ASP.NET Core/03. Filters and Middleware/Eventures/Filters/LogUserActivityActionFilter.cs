eb16819722e704753e2097b19ba032cf1985938b
T3ch0o <trottimusbg@gmail.com>
2018-11-20 15:21:34 +0200

    [LogUserActivityActionFilter] Overwritten OnActionExecuted method which logs the User event.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Filters/LogUserActivityActionFilter.cs
@@ -0,0 +1,39 @@
+﻿namespace Eventures.Filters
+{
+    using System;
+    using System.Linq;
+
+    using Eventures.Models;
+    using Eventures.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Logging;
+
+    public class LogUserActivityActionFilter : ActionFilterAttribute
+    {
+        private readonly IEventService _eventService;
+
+        private readonly ILogger<LogUserActivityActionFilter> _logger;
+
+        public LogUserActivityActionFilter(IEventService eventService, ILogger<LogUserActivityActionFilter> logger)
+        {
+            _eventService = eventService;
+            _logger = logger;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            string username = context.HttpContext.User.Identity.Name;
+            Event eventureEvent = _eventService.All().FirstOrDefault();
+
+            if (eventureEvent != null)
+            {
+                string logMessage = $"[{DateTime.Now}] User {username} viewed event {eventureEvent.Name} ( {eventureEvent.Start} / {eventureEvent.End})";
+
+                _logger.LogInformation(logMessage);
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
