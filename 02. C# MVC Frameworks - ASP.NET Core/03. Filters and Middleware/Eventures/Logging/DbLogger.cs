4872aefd9a5c7d421e6aa8cda91993d09becc8dc
T3ch0o <trottimusbg@gmail.com>
2018-11-20 14:38:22 +0200

    [DbLogger] Initialize Log method which  save the error log to the database.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Logging/DbLogger.cs
@@ -0,0 +1,44 @@
+﻿namespace Eventures.Logging
+{
+    using System;
+
+    using Eventures.Data;
+    using Eventures.Models;
+
+    using Microsoft.Extensions.Logging;
+
+    public class DbLogger : ILogger
+    {
+        private readonly string _categoryName;
+
+        private readonly Func<string, LogLevel, bool> _filter;
+
+        private readonly EventuresDbContext _db;
+
+        public DbLogger(string categoryName, Func<string, LogLevel, bool> filter, EventuresDbContext db)
+        {
+            _categoryName = categoryName;
+            _filter = filter;
+            _db = db;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (logLevel == LogLevel.Error)
+            {
+                _db.Logs.Add(new CustomLog());
+                _db.SaveChanges();
+            }
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+}
