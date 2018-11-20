0dc5f12fdc03ba614ef171eee467fc59e238d503
T3ch0o <trottimusbg@gmail.com>
2018-11-20 22:17:47 +0200

    [LoginViewModel] Added properties validation.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ViewModels/LoginViewModel.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ViewModels/LoginViewModel.cs
@@ -6,11 +6,13 @@
     {
         [Required]
         [Display(Name = "Username")]
+        [MinLength(3, ErrorMessage = "Username is too short.")]
         public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [MinLength(3, ErrorMessage = "Password is too short.")]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
