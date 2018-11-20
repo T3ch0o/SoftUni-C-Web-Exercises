e4471823044f6ae0867caa8fa1eb7e6f8582b4bd
T3ch0o <trottimusbg@gmail.com>
2018-11-20 14:41:18 +0200

    [CustomLog] Created model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/CustomLog.cs
@@ -0,0 +1,17 @@
+﻿namespace Eventures.Models
+{
+    using System;
+
+    public class CustomLog
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public int EventId { get; set; }
+
+        public string LogLevel { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+    }
+}
