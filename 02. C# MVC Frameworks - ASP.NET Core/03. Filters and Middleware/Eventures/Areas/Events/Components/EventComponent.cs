06812a3d544b81ecd82cfe04af2fbd1a4314506d
T3ch0o <trottimusbg@gmail.com>
2018-11-20 15:36:52 +0200

    [EventComponent] Added component for getting all events and render the view.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Components/EventComponent.cs
@@ -0,0 +1,35 @@
+﻿namespace Eventures.Areas.Events.Components
+{
+    using System.Collections.Generic;
+
+    using Eventures.Areas.Event.ViewModels;
+    using Eventures.Models;
+    using Eventures.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class EventComponent : ViewComponent
+    {
+        private readonly IEventService _eventService;
+
+        public EventComponent(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            IEnumerable<Event> events = _eventService.All();
+            List<EventViewModel> eventViewModels = new List<EventViewModel>();
+
+            foreach (Event @event in events)
+            {
+                eventViewModels.Add(new EventViewModel { Name = @event.Name, Place = @event.Place, Start = @event.Start, End = @event.End });
+            }
+
+
+            return View(eventViewModels);
+        }
+
+    }
+}
