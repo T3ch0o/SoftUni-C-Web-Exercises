bee01ff2e07df134e3f00a851366d520ab54df2e
T3ch0o <trottimusbg@gmail.com>
2018-11-20 14:39:49 +0200

    [LoggerFactoryExtensions] Extends ILoggerFactory.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Logging/LoggerFactoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Eventures.Logging
+{
+    using System;
+
+    using Eventures.Data;
+
+    using Microsoft.Extensions.Logging;
+
+    public static class LoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddContext(this ILoggerFactory factory, Func<string, LogLevel, bool> filter, EventuresDbContext db)
+        {
+            factory.AddProvider(new DbLoggerProvider(filter, db));
+            return factory;
+        }
+
+        public static ILoggerFactory AddContext(this ILoggerFactory factory, LogLevel minLevel, EventuresDbContext db)
+        {
+            return AddContext(factory, (_, logLevel) => logLevel >= minLevel, db);
+        }
+    }
+}
