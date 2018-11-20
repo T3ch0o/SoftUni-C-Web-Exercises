b9176a7bb47ecabb21a96d85b151d5f5cf7457b2
T3ch0o <trottimusbg@gmail.com>
2018-11-20 14:41:02 +0200

    [DbLoggerProvider] Implemented CreateLogger method and return new DbLogger.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Logging/Providers/DbLoggerProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace Eventures.Logging
+{
+    using System;
+
+    using Eventures.Data;
+
+    using Microsoft.Extensions.Logging;
+
+    public class DbLoggerProvider : ILoggerProvider
+    {
+        private readonly Func<string, LogLevel, bool> _filter;
+
+        private readonly EventuresDbContext _db;
+
+        public DbLoggerProvider(Func<string, LogLevel, bool> filter, EventuresDbContext db)
+        {
+            _filter = filter;
+            _db = db;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new DbLogger(categoryName, _filter, _db);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
