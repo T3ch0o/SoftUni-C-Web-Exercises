55f2a05a7600b44b5621f3f8828ae99aeb6fdcd4
T3ch0o <trottimusbg@gmail.com>
2018-12-03 15:46:37 +0200

    [OrderViewModel] Create model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Eventures.Models.ViewModels
+{
+    using System;
+
+    public class OrderViewModel
+    {
+        public string EventName { get; set; }
+
+        public string Customer { get; set; }
+
+        public DateTime OrderOn { get; set; }
+    }
+}
