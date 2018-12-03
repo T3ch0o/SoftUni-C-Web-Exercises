0c04361bdb1766bb36c694d0617ab2b3db00015b
T3ch0o <trottimusbg@gmail.com>
2018-12-03 14:11:09 +0200

    [MyOrderedEventsViewModel] Added model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/ViewModels/MyOrderedEventsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Eventures.Areas.Events.ViewModels
+{
+    using System;
+
+    public class MyOrderedEventsViewModel
+    {
+        public string Name { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        public int TicketsCount { get; set; }
+    }
+}
