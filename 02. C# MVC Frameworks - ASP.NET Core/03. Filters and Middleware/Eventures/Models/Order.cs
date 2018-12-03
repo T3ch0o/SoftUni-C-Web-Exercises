0de78598d24fcfa76f6845baf3926d58987d7bb8
T3ch0o <trottimusbg@gmail.com>
2018-12-03 12:26:39 +0200

    [Order] Created model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace Eventures.Models
+{
+    using System;
+
+    public class Order
+    {
+        public string Id { get; set; }
+
+        public DateTime OrderOn { get; set; }
+
+        public int TicketsCount { get; set; }
+
+        public int EventId { get; set; }
+
+        public string CustomerId { get; set; }
+    }
+}
