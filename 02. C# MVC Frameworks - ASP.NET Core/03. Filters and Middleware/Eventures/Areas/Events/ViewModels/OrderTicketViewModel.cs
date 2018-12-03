918e6c1362b539dadebd76d8596ffb1ee5ccc111
T3ch0o <trottimusbg@gmail.com>
2018-12-03 12:24:33 +0200

    [OrderTicketViewModel] Created for order tickets in All Events action.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/ViewModels/OrderTicketViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Eventures.Areas.Events.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class OrderTicketViewModel
+    {
+        [Required]
+        [Display(Name = "Tickets")]
+        [Range(0, int.MaxValue, ErrorMessage = "Tickets should be a positive number.")]
+        public int Tickets { get; set; }
+    }
+}
