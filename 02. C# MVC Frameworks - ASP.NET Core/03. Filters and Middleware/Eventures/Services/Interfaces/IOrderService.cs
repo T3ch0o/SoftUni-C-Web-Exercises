749b9c77bc8448bc8a3a27a72f4517b4be2bb8ff
T3ch0o <trottimusbg@gmail.com>
2018-12-03 15:47:59 +0200

    [OrderSerivce] Created method to GetAllOrders.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/Interfaces/IOrderService.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/Interfaces/IOrderService.cs
@@ -1,9 +1,14 @@
 ﻿namespace Eventures.Services.Interfaces
 {
+    using System.Collections.Generic;
+
     using Eventures.Areas.Events.ViewModels;
+    using Eventures.Models.ViewModels;
 
     public interface IOrderService
     {
+        List<OrderViewModel> GetAllOrder();
+
         void CreateOrder(OrderTicketViewModel model, string customerId);
     }
 }
--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/OrderService.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Services/OrderService.cs
@@ -1,11 +1,16 @@
 ﻿namespace Eventures.Services
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     using Eventures.Areas.Events.ViewModels;
     using Eventures.Data;
     using Eventures.Models;
+    using Eventures.Models.ViewModels;
     using Eventures.Services.Interfaces;
+
+    using Microsoft.EntityFrameworkCore;
 
     public class OrderService : IOrderService
     {
@@ -14,6 +19,23 @@
         public OrderService(EventuresDbContext db)
         {
             _db = db;
+        }
+
+        public List<OrderViewModel> GetAllOrder()
+        {
+            List<OrderViewModel> orders = new List<OrderViewModel>();
+
+            foreach (Order order in _db.Orders.Include(order => order.Customer).Include(order => order.Event))
+            {
+                orders.Add(new OrderViewModel
+                {
+                     EventName = order.Event.Name,
+                     Customer = order.Customer.UserName,
+                     OrderOn = order.OrderOn
+                });
+            }
+
+            return orders;
         }
 
         public void CreateOrder(OrderTicketViewModel model, string customerId)
