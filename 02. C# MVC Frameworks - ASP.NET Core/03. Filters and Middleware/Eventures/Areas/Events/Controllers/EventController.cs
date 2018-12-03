15bbb7d32e8d1de94796a4772a07075e384578f1
T3ch0o <trottimusbg@gmail.com>
2018-12-03 14:10:23 +0200

    [EventController] Added MyEvents Action.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Controllers/EventController.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/Areas/Events/Controllers/EventController.cs
@@ -1,6 +1,11 @@
 ﻿namespace Eventures.Areas.Events.Controllers
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+
     using Eventures.Areas.Event.ViewModels;
+    using Eventures.Areas.Events.ViewModels;
     using Eventures.Filters;
     using Eventures.Services.Interfaces;
 
@@ -12,15 +17,42 @@
     {
         private readonly IEventService _eventService;
 
-        public EventController(IEventService eventService)
+        private readonly IOrderService _orderService;
+
+        public EventController(IEventService eventService, IOrderService orderService)
         {
             _eventService = eventService;
+            _orderService = orderService;
         }
 
         [ServiceFilter(typeof(LogUserActivityActionFilter))]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(OrderTicketViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                _orderService.CreateOrder(model, userId);
+
+                return RedirectToAction("MyEvents");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult MyEvents()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            List<MyOrderedEventsViewModel> myEvents = _eventService.GetOrderedEvents(userId);
+
+            return View(myEvents);
         }
 
         [Authorize(Roles = "Administrator")]
