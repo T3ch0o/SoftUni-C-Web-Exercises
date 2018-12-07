8be28f79f0ed3fb7701e0d69aeedc9d63aaefbf4
T3ch0o <trottimusbg@gmail.com>
2018-12-07 15:09:27 +0200

    [MappingConfiguration] Created configurations for auto mapping properties.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Filters and Middleware/Eventures/MappingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Eventures
+{
+    using AutoMapper;
+
+    using Eventures.Areas.Event.ViewModels;
+    using Eventures.Models;
+    using Eventures.Models.ViewModels;
+
+    public class MappingConfiguration : Profile
+    {
+        public MappingConfiguration()
+        {
+            CreateMap<RegisterViewModel, ApplicationUser>();
+            CreateMap<Event, EventViewModel>();
+        }
+    }
+}
