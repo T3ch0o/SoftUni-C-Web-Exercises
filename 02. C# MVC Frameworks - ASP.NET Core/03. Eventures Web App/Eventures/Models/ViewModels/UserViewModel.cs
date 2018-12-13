a1919261b13e3218484fd1010ebf7f9db470a028
T3ch0o <trottimusbg@gmail.com>
2018-12-13 22:13:30 +0200

    [UserViewModel] Created model.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Eventures Web App/Eventures/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Eventures.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
