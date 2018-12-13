2dc05b7d0f1558f004e6111332e365a67b2182c4
T3ch0o <trottimusbg@gmail.com>
2018-12-13 22:14:22 +0200

    [UserService] Implemented GetAll users and ChangeUserRole service method.

---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Eventures Web App/Eventures/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿namespace Eventures.Services.Interfaces
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Eventures.Models;
+
+    public interface IUserService
+    {
+        IQueryable<ApplicationUser> GetAll(string currentUser);
+
+        Task ChangeUserRole(string userData);
+    }
+}
---
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Eventures Web App/Eventures/Services/UserService.cs
@@ -0,0 +1,46 @@
+﻿namespace Eventures.Services
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Eventures.Data;
+    using Eventures.Models;
+    using Eventures.Services.Interfaces;
+
+    using Microsoft.AspNetCore.Identity;
+
+    public class UserService : IUserService
+    {
+        private readonly EventuresDbContext _db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(EventuresDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IQueryable<ApplicationUser> GetAll(string currentUser)
+        {
+
+            return _db.Users.Where(u => u.UserName != currentUser);
+        }
+
+        public async Task ChangeUserRole(string userData)
+        {
+            string[] userInfo = userData.Split(" ");
+
+            ApplicationUser user = _db.Users.FirstOrDefault(u => u.Id == userInfo[0]);
+
+            if (userInfo[1] != "Admin")
+            {
+                await _userManager.AddToRoleAsync(user, "Administrator");
+            }
+            else
+            {
+                await _userManager.RemoveFromRoleAsync(user, "Administrator");
+            }
+        }
+    }
+}
