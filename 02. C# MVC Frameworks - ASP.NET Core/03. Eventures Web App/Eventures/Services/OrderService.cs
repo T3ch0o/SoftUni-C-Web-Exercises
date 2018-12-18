b715a2eb779a31c6c7b439689e487d4293bfd8a0
T3ch0o <trottimusbg@gmail.com>
2018-12-18 12:38:38 +0200

    [OrderService] Removed automapper.

--- 02. C# MVC Frameworks - ASP.NET Core/03. Eventures Web App/Eventures/Services/OrderService.cs
+++ 02. C# MVC Frameworks - ASP.NET Core/03. Eventures Web App/Eventures/Services/OrderService.cs
@@ -4,8 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    using AutoMapper;
-
     using Eventures.Areas.Events.ViewModels;
     using Eventures.Data;
     using Eventures.Models;
